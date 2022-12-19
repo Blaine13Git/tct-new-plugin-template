5151b62c9ee01596510534e1d47518386eeda294
GitHub Action <action@github.com>
2022-12-19 08:26:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/blaine13git/tctnewplugintemplate/MyBundle.kt
+++ src/main/kotlin/com/github/blaine13git/tctnewplugintemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.blaine13git.tctnewplugintemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/blaine13git/tctnewplugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/blaine13git/tctnewplugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.blaine13git.tctnewplugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.blaine13git.tctnewplugintemplate.MyBundle
 
 class MyApplicationService {
 
